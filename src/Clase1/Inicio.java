a547a87a6f65026da8c3b4a39353cc7ca4ecda48
Estudiantes <rogerbetan08@gmail.com>
2024-08-10 12:45:03 -0500

    Nuevo actu

--- src/Clase1/Inicio.java
+++ src/Clase1/Inicio.java
@@ -1,4 +1,5 @@
 package Clase1;
+import java.util.Scanner;
 
 public class Inicio {
 
@@ -6,12 +7,23 @@ public class Inicio {
 
         int x = 22;
 
+        Scanner sc = new Scanner (System.in);
+
         if(x >= 18){
             System.out.println("Es mayor de edad");
         }
         else {
             System.out.println("Eres menor de edad");
         }
+
+
+        System.out.println("Ingrese su nombre");
+        String Nombre = sc.nextLine();
+
+        System.out.println("Ingrese su edad ");
+        int edad_=sc.nextInt();
+
+        System.out.println("Su edad es  de "+edad_);
 
 
     }
