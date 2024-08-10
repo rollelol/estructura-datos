2366fb4e4849bc0af35d56362e44affe2b6e6c72
Estudiantes <rogerbetan08@gmail.com>
2024-08-10 12:07:51 -0500

    Variable

---
+++ src/Clase1/Inicio.java
@@ -0,0 +1,22 @@
+package Clase1;
+
+public class Inicio {
+
+    public static void main(String[] args){
+
+        int edad = 22;
+
+        if (edad >= 18){
+            System.out.println("Puedes entrar");
+        }
+        else {
+            System.out.println("No puedes entrar");
+        }
+
+
+
+    }
+
+
+}
+
